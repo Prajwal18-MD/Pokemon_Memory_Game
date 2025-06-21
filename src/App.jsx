c9213a7a11d91bb56525433097fa6e66941872ef
Prajwal-18MD <prajju.18gryphon@gmail.com>
2025-06-21 20:26:01 +0530

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
   const fetchPokemon = async() => {
     const pokemonList = [];
-    for(let i = 1; i <= 12; i++){
-      const response = await fetch(`https://pokeapi.co/api/v2/${i}`);
+    for(let i = 1; i <= 300; i++){
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
       const data = await response.json();
       pokemonList.push({
         id : data.id,
@@ -41,7 +41,7 @@ function App() {
       setScore(score + 1);
       setBestScore(Math.max(bestScore, score + 1))
 
-       if(score + 1 === 12){
+       if(score + 1 === 300){
        alert("You've Won!");
        setClickedPokemon([]);
        setScore(0);
@@ -59,19 +59,21 @@ function App() {
          <div className='flex flex-col items-center justify-center text-center backdrop-blur-xl bg-black/70 p-3 sm:p-5 rounded-2xl'>
           <h1 className='font-mono font-bold text-3xl text-white sm:text-4xl md:text-5xl mb-2 sm:mb-3 p-2 sm:p-5'>POKEMON MEMORY GAME</h1>
           <div className='mb-3 sm:mb-5 text-white text-lg sm:text-xl md:text-2xl'>
-            <p>Score: </p>
-            <p>Best Score: </p>
+            <p>Score: {score}</p>
+            <p>Best Score: {bestScore}</p>
           </div>
          </div>
          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 sm:gap-4 w-full max-w-3xl">
           {
-            pokemon.map((p) =>{
-              <div className="bg-white rounded-lg shadow-lg p-2 sm:p-4 cursor-pointer transform hover:scale-105 transition-transform" key={p.id}>
-                <img src={p.image} alt="" />
-                <p>{p.name}</p>
+            pokemon.map((p) =>(
+              <div className="bg-white rounded-lg shadow-lg p-2 sm:p-4 cursor-pointer transform hover:scale-105 transition-transform"
+               key={p.id} onClick={() => handleClick(p.id)}>
+                <img src={p.image} alt=""
+                className='w-16 h-16 sm:w-24 sm:h-24 md:w-32 md:h-32 mx-auto' />
+                <p className='text-center mt-1 sm:mt-2 text-sm sm:text-base'>{p.name}</p>
               </div>
               
-            })
+            ))
           }
 
          </div>
